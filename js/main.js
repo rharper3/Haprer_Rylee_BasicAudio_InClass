28004135e0841ccfdae7b5253f735dd03f7650c0
Harper <r_harper3@tss-hr420-dm08.fc.ca>
2019-02-25 15:44:45 -0500

    made it so taht the audio switched upon click of a image

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@
 // create a variable stack and grab all of the DOM elements form the page
 
 	let aud =		document.querySelector("audio"),
-		play = 		document.querySelector("#playButton")
-		pause = 	document.querySelector("#pauseButton")
-		rewind = 	document.querySelector("#rewindButton");
+		play = 		document.querySelector("#playButton"),
+		pause = 	document.querySelector("#pauseButton"),
+		rewind = 	document.querySelector("#rewindButton"),
+		tracks =	document.querySelectorAll(".trackHolder");
 
 // write functions got the audio element
 	function playAudio() {
@@ -20,9 +21,19 @@
 		aud.currentTime = 0;
 	}
 
+	function swapSource() {
+		let currentTrack = this.dataset.currenttrack;
+
+		aud.src = `audio/${currentTrack}`;
+		aud.load();
+		aud.play();
+	}
+
 // add event handling
 	play.addEventListener("click", playAudio);
 	pause.addEventListener("click", pauseAudio);
 	rewind.addEventListener("click", rewindAudio);
+
+	tracks.forEach(track => track.addEventListener("click", swapSource));
 
 })();
